d9be346f0c416d5c867065264e02646c010042c7
Ramzza <thezniqer@gmail.com>
2024-06-14 15:37:14 +0300

    feat: check own reservations with default cookie

--- get_own_reservations.sh
+++ get_own_reservations.sh
@@ -5,6 +5,14 @@ SCRIPT_DIR="$(CDPATH= cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Specify the path to your.env file relative to the script directory
 ENV_FILE="$SCRIPT_DIR/.env"
+log_file="$SCRIPT_DIR/$(basename "$0" .sh).log"
+
+# Function to prepend current date and time to log messages
+log_with_date() {
+    echo "$(date) - R: $1" | tee -a $log_file
+}
+
+log_with_date "$(basename "$0") script started"
 
 # Check if the.env file exists
 if [ -f "$ENV_FILE" ]; then
@@ -31,12 +39,19 @@ fi
 
 # Check if exactly one argument (the cookie value) is provided
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <cookie_value>"
-    exit 1
+    if [ -z "$CONST_COOKIE" ]; then
+        log_with_date "Usage: $0 <cookie_value>"
+        log_with_date "Or set CONST_COOKIE environment variable."
+        exit 1
+    else
+        cookie_value="$CONST_COOKIE"
+    fi
+else
+    # Assign the first argument to a variable
+    cookie_value="$1"
 fi
-
-# Assign the first argument to a variable
-cookie_value="$1"
 
 # Execute the GET request with the provided cookie and return the result
 curl -s -H "cookie: client_session=$cookie_value" "$CONST_URL_OWN_RESERVATION" | jq -r '.appointments[] | "\(.appointment_date) \(.appointment_time) \(.business_name)"'
+
+log_with_date "$(basename "$0") script finished"
